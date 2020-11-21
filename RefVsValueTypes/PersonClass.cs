925581fa4358b7ddf14b51d91d3cadc78a65b4eb
Martin Polansky <martinpol@youforce.net>
2020-11-21 20:33:13 +0100

    Added more info about GetHashcode() method and comparing two objects.

--- RefVsValueTypes/PersonClass.cs
+++ RefVsValueTypes/PersonClass.cs
@@ -4,13 +4,13 @@ namespace RefVsValueTypes
 {
     public class PersonClass
     {
-        // Reference types (class-es) are muttable: 
+        // Reference types (class-es) are muttable:
         // At the core of classic object-oriented programming is the idea that an object has a strong identity
         // and encapsulates mutable state that evolves over time. C# has always worked great for that, But
         // sometimes you want pretty much the exact opposite, and here C#’s defaults have tended to get in the way, making things very laborious. => What does it mean? => override GetHashcode method from the object base class as well as Equals method.
         // (in new DotNet 5.0 we have "record" type which solves the issue of muttability of the reference types (classes). A "record" is still a class but it is not possible to change it. DotNet takes care about instance comparison...)
         // Equality comparison works based on comparing equality for every property
-        // 
+
         public PersonClass(string firstName, string lastName, int age, Gender gender)
         {
             FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
--- RefVsValueTypes/Program.cs
+++ RefVsValueTypes/Program.cs
@@ -6,24 +6,29 @@ namespace RefVsValueTypes
     {
         // More info about the structs vs classes here:
         // https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/choosing-between-class-and-struct
+        // https://docs.microsoft.com/en-us/dotnet/api/system.object.gethashcode?view=net-5.0
         static void Main(string[] args)
         {
-            PersonClass martin1 = new PersonClass("Martin", "Polansky", 38, Gender.Male);
-            PersonClass martin2 = new PersonClass("Martin", "Polansky", 38, Gender.Male);
-            Console.WriteLine($"Is '{nameof(martin1)}' equal to '{nameof(martin2)}' : {martin1.Equals(martin2)}");
+            PersonClass person1 = new PersonClass("Martin", "Polansky", 38, Gender.Male);
+            PersonClass person2 = new PersonClass("Martin", "Polansky", 38, Gender.Male);
+            Console.WriteLine($"Is '{nameof(person1)}' equal to '{nameof(person2)}' : {person1.Equals(person2)}");
 
+            // Look into base object class methods (ToString(), GetHashcode(), Equals(), GetType())
             // What do we need to do in order to make those instances queal?
             // What hapens when we pass a type into a methos as a parameter? What? How is it copied?
             // Is reference equality the same thing like instance equality?
             // What is similar to a reference in lower level programming languages like C, C++?
+            // What does method GetHashcode() do?
+            // What's the difference between object.ReferenceEquals(obj1, obj2) vs object.Equals(obj1, obj2) vs obj.Equals(obj1)
+            // Equality oparators ???
 
-            PersonStruct martinA = new PersonStruct("Martin", "Polansky", 38, Gender.Male);
-            PersonStruct martinB = new PersonStruct("Martin", "Polansky", 38, Gender.Male);
-            Console.WriteLine($"Is '{nameof(martinA)}' equal to '{nameof(martinB)}' : {martinA.Equals(martinB)}");
+            PersonStruct personA = new PersonStruct("Martin", "Polansky", 38, Gender.Male);
+            PersonStruct personB = new PersonStruct("Martin", "Polansky", 38, Gender.Male);
+            Console.WriteLine($"Is '{nameof(personA)}' equal to '{nameof(personB)}' : {personA.Equals(personB)}");
 
             // Lets change martinA
-            martinA.UpdateAge(27);
-            Console.WriteLine($"Is '{nameof(martinA)}' equal to '{nameof(martinB)}' : {martinA.Equals(martinB)}");
+            personA.UpdateAge(27);
+            Console.WriteLine($"Is '{nameof(personA)}' equal to '{nameof(personB)}' : {personA.Equals(personB)}");
 
             // common value types (structs): int, uint, double, decimal, bool, byte, DateTime etc... => all primitives!
             // common ref types (every class we write) + string! (even if it behaves as a value type!)
